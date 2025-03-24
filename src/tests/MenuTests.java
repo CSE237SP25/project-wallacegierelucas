aaed36f7604bb6695aa690a64f1f138239e503ec
ErikaWallace12 <erikadwallace02@gmail.com>
2025-03-24 18:48:26 -0500

    updated test cases for menu interactions

--- src/tests/MenuTests.java
+++ src/tests/MenuTests.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 import bankapp.BankAccount;
@@ -13,7 +18,7 @@ import exceptions.InvalidMenuOptionException;
 public class MenuTests {
 	
 	@Test
-	public void testProcessDeposit() {
+	public void testUserDeposit() {
 		Menu menu = new Menu();
 		
 		menu.processDeposit(20);
@@ -23,7 +28,7 @@ public class MenuTests {
 	}
 	
 	@Test
-	public void testProcessWithdrawal() {
+	public void testUserWithdrawal() {
 		Menu menu = new Menu();
 		
 		menu.processDeposit(50);
@@ -31,6 +36,67 @@ public class MenuTests {
 		
 		BankAccount account = menu.getAccount();
 		assertEquals(account.getCurrentBalance(), 30.0, 0.005);
+	}
+	
+	@Test
+	public void testUserCheckBalance() {
+		PrintStream originalOutputStream = System.out;
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStream));
+		
+		Menu menu = new Menu();
+		menu.processDeposit(50);
+		menu.processCheckBalance();
+		
+		System.setOut(originalOutputStream);
+		
+		
+		assertEquals("Current Balance: 50.0", outputStream.toString().trim());
+	}
+	
+	@Test
+	void testProcessUserOptionInput_Deposit() {		
+		String input = "20\n";
+	    
+	    InputStream originalInputStream = System.in;
+	    InputStream testInputStream = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(testInputStream);
+	    
+	    // mutes the print statement in processUserOptionInput()
+		PrintStream originalOutputStream = System.out;
+		System.setOut(new PrintStream(new ByteArrayOutputStream()));
+		
+		Menu menu = new Menu();
+		menu.processUserOptionInput(2);
+		
+		System.setIn(originalInputStream);
+		System.setOut(originalOutputStream);
+
+		BankAccount account = menu.getAccount();
+	    assertEquals(20.0, account.getCurrentBalance(), 0.005);
+	}
+	
+	@Test
+	void testProcessUserOptionInput_Withdrawal() {
+	    String input = "20\n";
+	    
+	    InputStream originalInputStream = System.in;
+	    InputStream testInputStream = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(testInputStream);
+	    
+	    // mutes the print statement in processUserOptionInput()
+		PrintStream originalOutputStream = System.out;
+		System.setOut(new PrintStream(new ByteArrayOutputStream()));
+		
+		Menu menu = new Menu();
+		menu.processDeposit(50);
+		menu.processUserOptionInput(3);
+
+		System.setIn(originalInputStream);
+		System.setOut(originalOutputStream);
+		
+		BankAccount account = menu.getAccount();
+	    assertEquals(30.0, account.getCurrentBalance(), 0.005);
 	}
 	
 	@Test
