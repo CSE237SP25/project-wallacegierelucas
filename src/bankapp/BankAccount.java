f2f2bd6d5613bd4467f5914323d3b054538a3935
Nicole Lucas <lucas.n@wustl.edu>
2025-03-25 16:20:37 -0500

    removed to merge

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -1,5 +1,6 @@
 package bankapp;
 
+import exceptions.InsufficientFundsException;
 
 public class BankAccount {
     private double balance;
@@ -10,26 +11,6 @@ public class BankAccount {
             throw new IllegalArgumentException("Initial balance cannot be negative.");
         }
         this.balance = initialBalance;
-    }
-
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Deposit amount must be positive.");
-        }
-        this.balance += amount;
-    }
-
-    public void withdraw(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Withdrawal amount must be positive.");
-        }
-        if (balance < amount) {
-            throw new IllegalArgumentException("Insufficient funds.");
-        }
-    }
-
-    public double getCurrentBalance() {
-        return this.balance;
     }
 
     @Override
