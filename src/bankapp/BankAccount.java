f6beb7d632055291460e4980c53eb0cd2144e52e
Nicole Lucas <lucas.n@wustl.edu>
2025-04-07 09:22:14 -0500

    transaction update

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -1,12 +1,15 @@
 package bankapp;
 
 import exceptions.InsufficientFundsException;
+import java.util.ArrayList; 
+import java.util.List;
 
 public class BankAccount {
 	
 	private double balance;
     private String type;
     private String accountId;
+    private List<String> transactionHistory;
 
     public BankAccount(double initialBalance, String type, String accountId) {
         if (initialBalance < 0) {
@@ -15,6 +18,8 @@ public class BankAccount {
         this.balance = initialBalance;
         this.type = type;
         this.accountId = accountId;
+        this.transactionHistory = new ArrayList<>(); 
+        addTransaction("Account created with ID: " + accountId);
     }
   	
 	public BankAccount() {
@@ -26,6 +31,7 @@ public class BankAccount {
 			throw new IllegalArgumentException("Must deposit a positive amount.");
 		}
 		this.balance += amount;
+		addTransaction("Deposited $ "+ amount + " | New Balance: $" + balance);
 	}
 	
 	public void withdraw(double amount) {
@@ -38,10 +44,19 @@ public class BankAccount {
 		}
 		
 		this.balance -= amount;
+		addTransaction("Withdrew $" + amount + " | New Balance: $" + balance);
 	}
 	
 	public double getCurrentBalance() {
 		return this.balance;
+	}
+	
+	public void addTransaction(String description) { 
+		transactionHistory.add(description);
+	}
+	
+	public List<String> getTransactionHistory(){ 
+		return transactionHistory;
 	}
 	
     @Override
